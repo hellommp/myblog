2ca27f886411e3ec8249f675f0e0477609683ebd
hellommp <1747512087@qq.com>
2018-05-15 13:48:55 +0800

    尝试修改

--- src/main/java/com/tianli/web/AdminController.java
+++ src/main/java/com/tianli/web/AdminController.java
@@ -6,8 +6,11 @@
  */
 package com.tianli.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tianli.service.UserService;
 
 /** 
  * @Description: TODO
@@ -17,5 +20,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
+	
 }
