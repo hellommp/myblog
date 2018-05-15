2f396d80d9fcf515e4177d3416c0ae6764792fba
hellommp <1747512087@qq.com>
2018-05-15 13:14:43 +0800

    新增代码

---
+++ src/main/java/com/tianli/dao/BlogDao.java
@@ -0,0 +1,16 @@
+/**   
+ * 
+ * @author: TianLi   
+ *
+ * @date: 2018年5月15日 下午1:04:47 
+ */
+package com.tianli.dao;
+
+/** 
+ * @Description: TODO
+ * @author: TianLi
+ * @date: 2018年5月15日 下午1:04:47  
+ */
+public interface BlogDao {
+
+}
--- src/main/java/com/tianli/entity/Comment.java
+++ src/main/java/com/tianli/entity/Comment.java
@@ -22,6 +22,8 @@ public class Comment {
 	private Date createDate;
 	
 	private User user;
+	
+	private Blog blog;
 
 	public int getId() {
 		return id;
